f7ecdfda683803136944aa3b234b3493429b49c7
MahanNilipour <mahannilipour@gmail.com>
2025-03-18 13:24:36 -0400

    my work

--- src/Projects.js
+++ src/Projects.js
@@ -1,3 +1,7 @@
+import cat from "./images/nyan-cat.png" 
+import sling from "./images/slingair.png"
+import tic from "./images/tic-tac-toe.png"
+
 const Projects = () => {
     const imageSize= {
         maxWidth: '100%',
@@ -17,17 +21,17 @@ const Projects = () => {
             <div style={imageContainer}>
                 <h3>Nyan cat game</h3>
                 <a href="https://mahannilipour.github.io/nyan-cat/">
-                <img src="./images/nyan-cat.png" alt="project 1 screenshot" style={imageSize}/>
+                <img src={cat} alt="project 1 screenshot" style={imageSize}/>
                 </a>
                 <p>Built using HTML, CSS, and Javascript</p>
                 <h3>My first group project, Sling Air, which involved developing a reservation system with create, update, and delete functionality</h3>
                 <a href="https://github.com/MahanNilipour/slingair/compare/main%40%7B1day%7D...main">
-                <img src="./images/slingair.png" alt="project 2 screenshot" style={imageSize}/>
+                <img src= {sling} alt="project 2 screenshot" style={imageSize}/>
                 </a>
                 <p>Built using HTML, CSS, Javascript, NodeJS, Express, Insomnia, and MongoDB</p>
                 <h3>Tic-Tac-Toe game</h3>
                 <a href="https://mahannilipour.github.io/tic-tac-toe/">
-                <img src="./images/tic-tac-toe.png" alt="project 3 screenshot" style={imageSize}/>
+                <img src= {tic} alt="project 3 screenshot" style={imageSize}/>
                 </a>
                 <p>Built using HTML, CSS, Javascript, NodeJS, and React</p>
             </div>
